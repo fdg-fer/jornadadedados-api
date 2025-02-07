40d86576001b7dca52f57eb015d997fd0ecc7eba
fdg-fer <fdiasguima@gmail.com>
2025-02-07 17:26:27 -0300

    funcao requisicao ok

--- pipeline_00.py
+++ pipeline_00.py
@@ -4,11 +4,11 @@ import requests
 def extrair_dados_bitcoin():
     url = 'https://api.coinbase.com/v2/prices/spot'
     resposta = requests.get(url)
-    dados = resposta.json 
+    dados = resposta.json()
     return dados 
 
-if __name__=="__main":
-    print(extrair_dados_bitcoin)
+if __name__ == "__main__":
+    print(extrair_dados_bitcoin())
     
     
     
